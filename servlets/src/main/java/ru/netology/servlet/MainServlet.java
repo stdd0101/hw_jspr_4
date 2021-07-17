84fb5bf49b7df312cb999e41733c31ce96ab6f7f
stdd01 <stdd01@mail.ru>
2021-07-17 13:32:52 +0300

    add feature/di-java

--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,8 +1,8 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
-import ru.netology.model.Post;
-import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
 
 import javax.servlet.ServletException;
@@ -16,9 +16,17 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        // отдаём класс конфигурации
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+
+        // получаем по имени бина
+        final var controller = context.getBean("postController");
+
+        // получаем по классу бина
+        final var service = context.getBean(PostService.class);
+
+        // по умолчанию создаётся лишь один объект на BeanDefinition
+        final var isSame = service == context.getBean("postService");
     }
 
     @Override
