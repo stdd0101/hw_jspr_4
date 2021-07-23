7f792326cb15bd25470f0d2b106bab14219dfb28
stdd01 <stdd01@mail.ru>
2021-07-23 13:34:45 +0300

    refactor

--- servlets/src/main/java/ru/netology/Main.java
+++ servlets/src/main/java/ru/netology/Main.java
@@ -9,26 +9,26 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-//    public static void Main(String [] args) throws ClassNotFoundException, NoSuchMethodException, IllegalStateException,
-//            InvocationTargetException, IllegalAccessException, InstantiationException, ServletException, IOException {
-//        Class<?> aClass = Class.forName("ru.netology.servlet.MainServlet");
-//        Servlet servlet = (Servlet) aClass.getConstructor().newInstance();
-//        servlet.init(null);
-//        servlet.service(null, null);
-//        servlet.destroy();
-//    }
-
-    public static void main(String[] args) {
-        // отдаём список пакетов, в которых нужно искать аннотированные классы
-        final var context = new AnnotationConfigApplicationContext("ru.netology");
-
-        // получаем по имени бина
-        final var controller = context.getBean("postController");
-
-        // получаем по классу бина
-        final var service = context.getBean(PostService.class);
-
-        // по умолчанию создаётся лишь один объект на BeanDefinition
-        final var isSame = service == context.getBean("postService");
+    public static void main(String [] args) throws ClassNotFoundException, NoSuchMethodException, IllegalStateException,
+            InvocationTargetException, IllegalAccessException, InstantiationException, ServletException, IOException {
+        Class<?> aClass = Class.forName("ru.netology.servlet.MainServlet");
+        Servlet servlet = (Servlet) aClass.getConstructor().newInstance();
+        servlet.init(null);
+        //servlet.service(null, null);
+        //servlet.destroy();
     }
+
+//    public static void main(String[] args) {
+//        // отдаём список пакетов, в которых нужно искать аннотированные классы
+//        final var context = new AnnotationConfigApplicationContext("ru.netology");
+//
+//        // получаем по имени бина
+//        final var controller = context.getBean("postController");
+//
+//        // получаем по классу бина
+//        final var service = context.getBean(PostService.class);
+//
+//        // по умолчанию создаётся лишь один объект на BeanDefinition
+//        final var isSame = service == context.getBean("postService");
+//    }
 }
