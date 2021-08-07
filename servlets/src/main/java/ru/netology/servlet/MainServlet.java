0d65cccfcc84c07ac2743095e93bad3acae90f4d
stdd01 <stdd01@mail.ru>
2021-08-07 12:41:20 +0300

    fix

--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -16,7 +16,7 @@ public class MainServlet extends HttpServlet {
     public void init() {
         final var context = new AnnotationConfigApplicationContext("ru.netology");
 
-        final var controller = context.getBean(PostController.class);
+        controller = context.getBean(PostController.class);
     }
 
     @Override
